6321ffdbc172066c0a8c68b0c4e4964e4454b9d7
Neo2505 <84080534+Neo2505@users.noreply.github.com>
2022-03-28 20:50:31 +0530

    temporary change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "change is from webhook5" );
+        System.out.println( "change is temporary" );
     }
 }
