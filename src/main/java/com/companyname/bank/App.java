7e3fa29d06c46669290fdfcef7715df29a1f7a2f
Neo2505 <84080534+Neo2505@users.noreply.github.com>
2022-03-29 13:06:10 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "this is poll scm4" );
+        System.out.println( "this is github webhook pull" );
     }
 }
