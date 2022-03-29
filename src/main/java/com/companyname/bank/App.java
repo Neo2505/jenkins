1491a9b299ff128990c2be0371b574d5cfef57c4
Neo2505 <84080534+Neo2505@users.noreply.github.com>
2022-03-29 12:40:41 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "this is poll scm3" );
+        System.out.println( "this is poll scm4" );
     }
 }
