0effd8c5469b3f09a424f16185e1defa32a19770
Neo2505 <84080534+Neo2505@users.noreply.github.com>
2022-04-02 23:46:36 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "this is build remotely" );
+        System.out.println( "this is poll scm" );
     }
 }
